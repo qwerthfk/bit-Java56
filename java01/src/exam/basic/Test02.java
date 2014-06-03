48e2ecc8dda78797b06ce4baa21b7f6a911d2092
bit <bit@bit-PC>
2014-06-03 14:41:10 +0900

    d

---
+++ java01/src/exam/basic/Test02.java
@@ -0,0 +1,16 @@
+//2. 클래스
+/*
+ * classification (분류)
+ * 함수 --> 명령어들의 블럭
+ */
+
+package exam.basic;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
